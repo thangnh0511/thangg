f74c99cd0402844d4f76826e72709d057ab74139
thangnh0511 <thangnh0511@gmail.com>
2023-05-10 00:39:21 +0700

    Update md_header.js

--- profile-content/js/md_header.js
+++ profile-content/js/md_header.js
@@ -1,16 +1,16 @@
 export const DATA_PERSONAL = {
     "id": 1,
-      "name": "Nguyen Huu Thang",
-      "position": "IT Business Analyst ",
-      "status": "open",
-      "address": "Da Nang, Viet Nam",
-      "phoneNumber": "0906401845",
-      "email": "thangnh0511@gmail.com",
-      "linkedin": "https://www.linkedin.com/in/thang-nh/",
-      "lastUpdate": "09\/05\/2023",
-      "imageUrl": "https://firebasestorage.googleapis.com/v0/b/porfolio-c82b8.appspot.com/o/image%2F1632814895611.jpg?alt=media&token=878cf14e-3ddd-4e4e-8e91-eb7fd8da30b9",
-      "resumeUrl": "https://firebasestorage.googleapis.com/v0/b/porfolio-c82b8.appspot.com/o/file%2FBA_ThangNguyen_221012.pdf?alt=media&token=3c95ba4f-25ef-470b-9740-97c2aee4c314",
-      "version": "220402.0.10"
+    "name": "Nguyen Huu Thang",
+    "position": "IT Business Analyst ",
+    "status": "open",
+    "address": "Da Nang, Viet Nam",
+    "phoneNumber": "0906401845",
+    "email": "thangnh0511@gmail.com",
+    "linkedin": "https://www.linkedin.com/in/thang-nh/",
+    "lastUpdate": "09\/05\/2023",
+    "imageUrl": "https://firebasestorage.googleapis.com/v0/b/porfolio-c82b8.appspot.com/o/image%2F1632814895611.jpg?alt=media&token=878cf14e-3ddd-4e4e-8e91-eb7fd8da30b9",
+    "resumeUrl": "https://firebasestorage.googleapis.com/v0/b/porfolio-c82b8.appspot.com/o/file%2FBA_ThangNguyen_221012.pdf?alt=media&token=3c95ba4f-25ef-470b-9740-97c2aee4c314",
+    "version": "220402.0.10"
 }
 
 // ------------------------------
